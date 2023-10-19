044c842a7d80c502bd2411a7d554ebec834a7e15
Syedmasood-git <masoodhussaini46@gmail.com>
2023-10-19 17:57:46 +0530

    submitting code-Thu Oct 19 2023 17:57:45 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
+import Inputbox from "../Components/Inputbox";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Inputbox/>
     </div>
   )
 }
